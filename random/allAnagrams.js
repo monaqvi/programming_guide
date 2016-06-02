d2a070b587b62e494ab9b0b54fdc7b46763f7ee4
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-06-02 15:56:33 -0700

    Finish using forEach and strings

--- random/allAnagrams.js
+++ random/allAnagrams.js
@@ -13,25 +13,22 @@
   */
 
 var allAnagrams = function(string) {
-  // for strings, would use charAt because strings aren't accessible like arrays in IE (prior to IE9).
-  // use arrays for functional programming style
-
   // create a storage for words
   var anagrams = {};
 
   // create a function that can be called recursively with comboSoFar, lettersLeft as inputs
   var anagramMaker = function(comboSoFar, lettersLeft) {
     // when there are no more lettersLeft, add comboSoFar to the store
-    if (lettersLeft.length === 0) anagrams[comboSoFar.join('')] = true;
+    if (lettersLeft === '') anagrams[comboSoFar] = true;
     // loop through each letter in the world so it can be the starting letter
-    for (var i = 0; i < lettersLeft.length; i++) {
+    lettersLeft.split('').forEach(function(letter, i) {
       // take letter out of lettersLeft and put it into comboSoFar
-      anagramMaker(comboSoFar.concat(lettersLeft[i]), lettersLeft.slice(0, i).concat(lettersLeft.slice(i + 1)));
-    }
+      anagramMaker(comboSoFar.concat(letter), lettersLeft.slice(0, i).concat(lettersLeft.slice(i + 1)));
+    });
   }
 
   // launch the recursive function with an empty array and string.split('')
-  anagramMaker([], string.split(''));
+  anagramMaker('', string);
   // return keys in storage
   return Object.keys(anagrams);
 };
