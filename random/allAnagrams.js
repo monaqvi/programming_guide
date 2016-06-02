23452e4374447257f572290497125210037821c7
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-06-02 14:47:49 -0700

    Complete function

--- random/allAnagrams.js
+++ random/allAnagrams.js
@@ -17,18 +17,23 @@ var allAnagrams = function(string) {
   // use arrays for functional programming style
 
   // create a storage for words
+  var anagrams = {};
 
   // create a function that can be called recursively with comboSoFar, lettersLeft as inputs
-
+  var anagramMaker = function(comboSoFar, lettersLeft) {
+    // when there are no more lettersLeft, add comboSoFar to the store
+    if (lettersLeft.length === 0) anagrams[comboSoFar.join('')] = true;
     // loop through each letter in the world so it can be the starting letter
-
+    for (var i = 0; i < lettersLeft.length; i++) {
       // take letter out of lettersLeft and put it into comboSoFar
-
-      // when there are no more lettersLeft, add comboSoFar to the store
+      anagramMaker(comboSoFar.concat(lettersLeft[i]), lettersLeft.slice(0, i).concat(lettersLeft.slice(i + 1)));
+    }
+  }
 
   // launch the recursive function with an empty array and string.split('')
-
+  anagramMaker([], string.split(''));
   // return keys in storage
+  return Object.keys(anagrams);
 };
 
  var anagrams = allAnagrams('abc');
