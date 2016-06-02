0a8a31fb175eb0d518b4ff58efbf79ba52319f1c
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-06-02 14:18:21 -0700

    Add pseudocode

--- random/allAnagrams.js
+++ random/allAnagrams.js
@@ -13,5 +13,23 @@
   */
 
 var allAnagrams = function(string) {
-  // Your code here.
+  // for strings, would use charAt because strings aren't accessible like arrays in IE (prior to IE9).
+  // use arrays for functional programming style
+
+  // create a storage for words
+
+  // create a function that can be called recursively with comboSoFar, lettersLeft as inputs
+
+    // loop through each letter in the world so it can be the starting letter
+
+      // take letter out of lettersLeft and put it into comboSoFar
+
+      // when there are no more lettersLeft, add comboSoFar to the store
+
+  // launch the recursive function with an empty array and string.split('')
+
+  // return keys in storage
 };
+
+ var anagrams = allAnagrams('abc');
+ console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
